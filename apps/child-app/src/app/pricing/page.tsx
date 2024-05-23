b59d199f9d3d68bbd707cead5877a9a801e08b5a
Sean Sun <sean@miscreants.com>
2024-05-23 16:03:32 -0400

    push to prod

--- apps/child-app/src/app/pricing/page.tsx
+++ apps/child-app/src/app/pricing/page.tsx
@@ -1,3 +1,3 @@
 export default function PricingPage(): JSX.Element {
-  return <div>Pricing page. This page is served from the child zone.</div>;
+  return <div>Pricing page woo Pricing page. This page is served from the child zone.</div>;
 }
